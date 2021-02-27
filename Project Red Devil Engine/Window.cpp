6ac26255aeac5b4b74b7114b2d43978680efbae3
justin kringstad <78430579+SithlordSparky@users.noreply.github.com>
2021-02-27 17:45:09 -0600

    Creating a Window
    Making the main Engine Window
    still in progress

---
+++ Project Red Devil Engine/Window.cpp
@@ -0,0 +1,27 @@
+#include "Window.h"
+
+bool Window::init()
+{
+    WNDCLASSEX wc;
+    wc.cbClsExtra = NULL;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.cbWndExtra = NULL;
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hInstance = NULL;
+    wc.lpszClassName = "RedEWindowClass";
+    wc.lpszMenuName = "";
+    wc.style = NULL;
+
+
+    ::RegisterClassEx()
+
+    return true;
+}
+
+bool Window::release()
+{
+    return true;
+}
---
+++ Project Red Devil Engine/Window.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <Windows.h>
+
+
+
+class Window
+{
+public:
+
+	bool init();
+	bool release();
+
+};
+
